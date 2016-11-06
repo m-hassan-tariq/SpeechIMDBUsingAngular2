c089ab8e3da4372cd541bc50ac80972fd9e23b70
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-11-05 21:55:14 -0500

    getting ready for production

--- SpeechIMDB/App_Start/BundleConfig.cs
+++ SpeechIMDB/App_Start/BundleConfig.cs
@@ -10,10 +10,7 @@ namespace SpeechIMDB
             bundles.Add(new StyleBundle("~/CoreCSS").Include(
                       "~/Content/assets/css/bootstrap.min.css",
                       "~/Content/assets/css/animate.min.css",
-                      "~/Content/assets/css/paper-dashboard.css",
-                      "~/Content/assets/css/font-awesome.min.css",
-                      "~/Content/assets/css/google-font.css",
-                      "~/Content/assets/css/themify-icons.css"));
+                      "~/Content/assets/css/paper-dashboard.css"));
 
         }
     }
